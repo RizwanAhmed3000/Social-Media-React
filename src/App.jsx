a3937f6f1b78e10398bb8acfa5502f5a5f357173
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-09-29 15:05:41 +0500

    signup page UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Home from "./pages/Home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profilePage/Profile";
+import Signup from "./pages/signup/Signup";
 
 
 function App() {
   return (
     // <Home />
     // <Profile />
-    <Login />
+    // <Login />
+    <Signup />
   );
 }
 
